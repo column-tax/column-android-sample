b24f831760dcf094a8f4b6f272b40f5d3c34bc46
billy1kaplan <billy@columntax.com>
2023-11-29 15:40:53 -0800

    Add ability to paste CT URL (#7)

--- app/src/main/java/com/example/column/MainActivity.kt
+++ app/src/main/java/com/example/column/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var urlEditText: EditText;
+    // Either set test URL here or paste one into your Android device
+    private var defaultUrl = "<user url>"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
@@ -14,12 +19,23 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        urlEditText = findViewById(R.id.urlEditText)
     }
 
     fun launchWebView(view: View) {
         val intent = Intent(applicationContext, WebViewActivity::class.java)
-        val url = "<user url>"
+        val url = getUrlText()
         intent.putExtra("url", url)
         startActivity(intent)
+    }
+
+    private fun getUrlText(): String {
+        var urlText = urlEditText.text.toString()
+
+        return if (urlText.isNullOrEmpty()) {
+            return defaultUrl
+        } else {
+            urlText
+        }
     }
 }
